26d9915a29121ac845be2aba626c2cb012121951
Caleb Oh <caleboh01@gmail.com>
2020-06-07 18:32:21 -0400

    added qi test

--- src/__test__/test.ts
+++ src/__test__/test.ts
@@ -173,9 +173,41 @@ test('test randic', () => {
     (fc.assert(
         fc.property(
             fc.stringOf(fc.constantFrom("A", "T", "C", "G"), 1, 100), (s) => {
-                for(let idx = 0; idx < s.length; idx++) {
+                for (let idx = 0; idx < s.length; idx++) {
                     let my_char = s.charAt(idx)
                     expect(randic(s)[1][idx] == randic_key.my_char)
+                }
+            }
+        )
+    ))
+})
+
+const qi_key: any = {
+    AA: 12,
+    AC: 4,
+    GT: 6,
+    AG: 0,
+    CC: 13,
+    CA: 5,
+    CG: 10,
+    TT: 15,
+    GG: 14,
+    GC: 11,
+    AT: 8,
+    GA: 1,
+    TG: 7,
+    TA: 9,
+    TC: 3,
+    CT: 2,
+}
+
+test('test qi', () => {
+    (fc.assert(
+        fc.property(
+            fc.stringOf(fc.constantFrom("A", "T", "C", "G"), 2, 100), (s) => {
+                for (let idx = 0; idx < s.length; idx++) {
+                    let my_char = s.charAt(idx) + s.charAt(idx + 1)
+                    expect(qi(s)[1][idx] == qi_key.my_char)
                 }
             }
         )
