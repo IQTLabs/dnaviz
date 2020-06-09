86725d16dd27cbf7870c2dae3b8e56663cb46817
Caleb Oh <caleboh01@gmail.com>
2020-06-09 10:49:58 -0400

    shortened yau test

--- src/__test__/yau.test.ts
+++ src/__test__/yau.test.ts
@@ -36,34 +36,29 @@ test('test yau of C', () => {
   ]);
 });
 
-test('test y value correctness for yau', () => {
+test('test x value correctness for yau', () => {
   fc.assert(
     fc.property(fc.stringOf(fc.constantFrom('A', 'a', 'T', 't', 'U', 'u', 'C', 'c', 'G', 'g'), 1, 10000), (s) => {
-      expect(
-        yau(s)[1][yau(s)[1].length - 1] ===
-          -(3 ** 0.5 / 2) * (s.split('A').length - 1 + (s.split('a').length - 1)) +
-            (3 ** 0.5 / 2) *
-              (s.split('T').length -
-                1 +
-                (s.split('t').length - 1) +
-                (s.split('U').length - 1) +
-                (s.split('u').length - 1)) +
-            0.5 * (s.split('C').length - 1 + (s.split('c').length - 1)) -
-            0.5 * (s.split('G').length - 1 + (s.split('g').length - 1)),
+      expect(0.5 * ((s.split('A').length - 1) + (s.split('a').length - 1) + (s.split('T').length - 1) + (s.split('t').length - 1) + (s.split('U').length - 1) + (s.split('u').length - 1)) + (3**0.5/2) * ((s.split('G').length - 1) + (s.split('g').length - 1) + (s.split('C').length - 1) + (s.split('c').length - 1)) ==
+          yau(s)[0][yau(s)[0].length - 1],
       ).toBe(true);
     }),
   );
 });
 
-test('test x value correctness for yau', () => {
+test('test y value correctness for yau', () => {
   fc.assert(
     fc.property(fc.stringOf(fc.constantFrom('A', 'a', 'T', 't', 'U', 'u', 'C', 'c', 'G', 'g'), 1, 10000), (s) => {
-      expect(
-        yau(s)[0][yau(s)[1].length - 1] ==
-          (3 ** 0.5 / 2) * (s.split('G').length - 1) +
-            s.replace(/[^C]/g, '').length +
-            0.5 * (s.replace(/[^A]/g, '').length + s.replace(/[^T]/g, '').length + s.replace(/[^U]/g, '').length),
+      expect(((3**0.5/2) * ((s.split('T').length - 1) + (s.split('t').length - 1) + (s.split('U').length - 1) + (s.split('u').length - 1) - (s.split('A').length - 1) - (s.split('a').length - 1)) + 0.5 * ((s.split('C').length - 1) + (s.split('c').length - 1) - (s.split('G').length - 1) - (s.split('g').length - 1))) == yau(s)[1][yau(s)[1].length - 1],
       ).toBe(true);
+    }),
+  );
+});
+
+test('test yau length', () => {
+  fc.assert(
+    fc.property(fc.stringOf(fc.constantFrom('A', 'a', 'T', 't', 'U', 'u', 'C', 'c', 'G', 'g'), 1, 10000), (s) => {
+      expect(yau(s)[0].length == s.length + 1).toBe(true);
     }),
   );
 });
