3c46939e692fc0cc4262fce8f64f456d1b2ca609
Benjamin Lee <benjamindlee@me.com>
2020-11-13 19:48:08 -0500

    Fix broken WASM imports

--- benchmarks/assemblyscript/src/functions.js
+++ benchmarks/assemblyscript/src/functions.js
@@ -1,112 +1,113 @@
 const {
- __alloc,
- __retain,
- __release,
- memory,
- squiggle,
- x_squiggle,
- y_squiggle,
- gates,
- y_qi,
- x_qi,
- yau,
- x_yau_bp,
- y_yau_bp,
- x_randic,
- y_randic,
+  __alloc,
+  __retain,
+  __release,
+  memory,
+  squiggle,
+  x_squiggle,
+  y_squiggle,
+  gates,
+  y_qi,
+  x_qi,
+  yau,
+  x_yau_bp,
+  y_yau_bp,
+  x_randic,
+  y_randic,
 } = require('./index');
 
 let alloc = __alloc;
 
 function __allocString(str) {
- const length = str.length;
- const ptr = alloc(length << 1, 1);
- const U16 = new Uint16Array(memory.buffer);
- for (let i = 0, p = ptr >>> 1; i < length; ++i) {
-  U16[p + i] = str.charCodeAt(i);
- }
- return ptr;
+  const length = str.length;
+  const ptr = alloc(length << 1, 1);
+  const U16 = new Uint16Array(memory.buffer);
+  for (let i = 0, p = ptr >>> 1; i < length; ++i) {
+    U16[p + i] = str.charCodeAt(i);
+  }
+  return ptr;
 } // Places string into modules memory. Assigns and returns a pointer
 
 function getFloat64Array(ptr) {
- const buffer = memory.buffer;
- const U32 = new Uint32Array(buffer);
- const bufPtr = U32[(ptr + 4) >>> 2];
- return new Float64Array(buffer, bufPtr, U32[(bufPtr - 4) >>> 2] >>> 3);
+  const buffer = memory.buffer;
+  const U32 = new Uint32Array(buffer);
+  const bufPtr = U32[(ptr + 4) >>> 2];
+  return new Float64Array(buffer, bufPtr, U32[(bufPtr - 4) >>> 2] >>> 3);
 } // Copies the modules array values into a JS array
 
-module.exports = function as_yau(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const outArrPtr = __retain(yau(inStrPtr, seq.length));
- const resultArr = getFloat64Array(outArrPtr);
- __release(outArrPtr);
- __release(inStrPtr);
- return resultArr;
-}
+module.exports.as_yau = function as_yau(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const outArrPtr = __retain(yau(inStrPtr, seq.length));
+  const resultArr = getFloat64Array(outArrPtr);
+  __release(outArrPtr);
+  __release(inStrPtr);
+  return resultArr;
+};
 
-module.exports = function as_qi(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const xPtr = __retain(x_qi(seq.length));
- const yPtr = __retain(y_qi(inStrPtr, seq.length));
- const x = getFloat64Array(xPtr);
- const y = getFloat64Array(yPtr);
- __release(xPtr);
- __release(yPtr);
- __release(inStrPtr);
- return [x, y];
-}
+module.exports.as_qi = function as_qi(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const xPtr = __retain(x_qi(seq.length));
+  const yPtr = __retain(y_qi(inStrPtr, seq.length));
+  const x = getFloat64Array(xPtr);
+  const y = getFloat64Array(yPtr);
+  __release(xPtr);
+  __release(yPtr);
+  __release(inStrPtr);
+  return [x, y];
+};
 
-module.exports = function as_yau_bp(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const xPtr = __retain(x_yau_bp(seq.length));
- const yPtr = __retain(y_yau_bp(inStrPtr, seq.length));
- const x = getFloat64Array(xPtr);
- const y = getFloat64Array(yPtr);
- __release(xPtr);
- __release(yPtr);
- __release(inStrPtr);
- return [x, y];
-}
+module.exports.as_yau_bp = function as_yau_bp(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const xPtr = __retain(x_yau_bp(seq.length));
+  const yPtr = __retain(y_yau_bp(inStrPtr, seq.length));
+  const x = getFloat64Array(xPtr);
+  const y = getFloat64Array(yPtr);
+  __release(xPtr);
+  __release(yPtr);
+  __release(inStrPtr);
+  return [x, y];
+};
 
-module.exports = function as_randic(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const xPtr = __retain(x_randic(seq.length));
- const yPtr = __retain(y_randic(inStrPtr, seq.length));
- const x = getFloat64Array(xPtr);
- const y = getFloat64Array(yPtr);
- __release(xPtr);
- __release(yPtr);
- __release(inStrPtr);
- return [x, y];
-}
+module.exports.as_randic = function as_randic(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const xPtr = __retain(x_randic(seq.length));
+  const yPtr = __retain(y_randic(inStrPtr, seq.length));
+  const x = getFloat64Array(xPtr);
+  const y = getFloat64Array(yPtr);
+  __release(xPtr);
+  __release(yPtr);
+  __release(inStrPtr);
+  return [x, y];
+};
 
-module.exports = function as_squiggle_two_array_output(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const xPtr = __retain(x_squiggle(seq.length));
- const yPtr = __retain(y_squiggle(inStrPtr, seq.length));
- const x = getFloat64Array(xPtr);
- const y = getFloat64Array(yPtr);
- __release(xPtr);
- __release(yPtr);
- __release(inStrPtr);
- return [x, y];
-}
+module.exports.as_squiggle_two_array_output = function as_squiggle_two_array_output(
+  seq,
+) {
+  const inStrPtr = __retain(__allocString(seq));
+  const xPtr = __retain(x_squiggle(seq.length));
+  const yPtr = __retain(y_squiggle(inStrPtr, seq.length));
+  const x = getFloat64Array(xPtr);
+  const y = getFloat64Array(yPtr);
+  __release(xPtr);
+  __release(yPtr);
+  __release(inStrPtr);
+  return [x, y];
+};
 
-module.exports = function as_squiggle(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const outArrPtr = __retain(squiggle(inStrPtr, seq.length));
- const resultArr = getFloat64Array(outArrPtr);
- __release(outArrPtr);
- __release(inStrPtr);
- return resultArr;
-}
+module.exports.as_squiggle = function as_squiggle(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const outArrPtr = __retain(squiggle(inStrPtr, seq.length));
+  const resultArr = getFloat64Array(outArrPtr);
+  __release(outArrPtr);
+  __release(inStrPtr);
+  return resultArr;
+};
 
-module.exports = function as_gates(seq) {
- const inStrPtr = __retain(__allocString(seq));
- const outArrPtr = __retain(gates(inStrPtr, seq.length));
- const resultArr = getFloat64Array(outArrPtr);
- __release(outArrPtr);
- __release(inStrPtr);
- return resultArr;
-}
-
+module.exports.as_gates = function as_gates(seq) {
+  const inStrPtr = __retain(__allocString(seq));
+  const outArrPtr = __retain(gates(inStrPtr, seq.length));
+  const resultArr = getFloat64Array(outArrPtr);
+  __release(outArrPtr);
+  __release(inStrPtr);
+  return resultArr;
+};
--- benchmarks/assemblyscript/src/index.js
+++ benchmarks/assemblyscript/src/index.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 const compiled = new WebAssembly.Module(
-  fs.readFileSync(__dirname + "/build/optimized.wasm")
+  fs.readFileSync(path.resolve(__dirname, '../build/optimized.wasm')),
 );
-const memory = new WebAssembly.Memory({ initial: 4900 })
+const memory = new WebAssembly.Memory({ initial: 4900 });
 const imports = {
   env: {
     memory,
     abort(_msg, _file, line, column) {
-      console.error("abort called at index.ts:" + line + ":" + column);
+      console.error('abort called at index.ts:' + line + ':' + column);
     },
   },
 };
-Object.defineProperty(module, "exports", {
+Object.defineProperty(module, 'exports', {
   get: () => new WebAssembly.Instance(compiled, imports).exports,
 });
